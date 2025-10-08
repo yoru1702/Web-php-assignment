6fe7d52dc72a5e6be15307092b3b4cee7586ad3f
Patcharapol <patcharapol1702@gmail.com>
2025-10-08 21:26:32 +0700

    03

--- include/config.inc.php
+++ include/config.inc.php
@@ -6,6 +6,13 @@
     $conn = mysqli_connect($serv, $user, $pass, $data);
     if (!$conn) {
         die("Error Connect! " . mysqli_connect_error());
+    $serv = "localhost";
+    $user = "root";
+    $pass = "";
+    $data = "assignment";
+    $conn = mysqli_connect($serv, $user, $pass, $data);
+    if (!$conn) {
+        die("Error Connect! " . mysqli_connect_error());
     }
 
 
